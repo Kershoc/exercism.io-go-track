59769425efbc7835a0b30810ad1d5f828b0d948f
Symeon Quimby <kershoc@gmail.com>
2020-09-22 23:35:15 -0400

    refactor in favor of pure math

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -2,29 +2,18 @@ package diffsquares
 
 //SquareOfSum returns (1 + 2 + ... + n)²
 func SquareOfSum(n int) (sum int) {
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
-	sum = sum * sum
-	return
+	// [n(n+1)/2]^2
+	sum = n * (n + 1) / 2
+	return sum * sum
 }
 
 //SumOfSquares returns 1² + 2² + ... + n²
-func SumOfSquares(n int) (sum int) {
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return
+func SumOfSquares(n int) int {
+	// Σn2 = [n(n+1)(2n+1)]/6
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 //Difference returns (1 + 2 + ... + n)² - (1² + 2² + ... + n²)
 func Difference(n int) int {
-	soq := 0
-	qos := 0
-	for i := 1; i <= n; i++ {
-		soq += i
-		qos += i * i
-	}
-
-	return soq*soq - qos
+	return SquareOfSum(n) - SumOfSquares(n)
 }
